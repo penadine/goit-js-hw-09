e88b5045f240703c1020793b7eb6f2550f849bd6
penadine <105946746+penadine@users.noreply.github.com>
2023-07-12 16:08:24 +0000

    Deploying to gh-pages from @ penadine/goit-js-hw-09@825944691198daf4ec51228971a338166e45f45d 🚀

---
+++ 01-color-switcher.73faab25.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),d=null;e.disabled=!0,t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,d=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){e.disabled=!0,t.disabled=!1,clearInterval(d)}))}();
+//# sourceMappingURL=01-color-switcher.73faab25.js.map
